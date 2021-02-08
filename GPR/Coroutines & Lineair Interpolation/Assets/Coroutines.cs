e5439e4bbf6ecd91733eb4a697c483fb428905bb
Emile <emiledavidson@gmail.com>
2021-02-08 12:03:18 +0100

    First asignment Coroutine finished

---
+++ GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    public IEnumerator Coroutine()
+    {
+        print("<color=green> Ik start nu de coroutine </color>");
+        yield return new WaitForSeconds(.5f);
+        print("<color=yellow> coroutine update </color>");
+        yield return new WaitForSeconds(.5f);
+        print("<color=red> coroutine einde </color>");
+        yield return null;
+    }
+}
