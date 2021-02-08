5cf3b011a4337447bbb8689ddf581258c6521297
Emile <emiledavidson@gmail.com>
2021-02-08 12:27:33 +0100

    third assignment finished

--- GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
+++ GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coroutines : MonoBehaviour
 {
+    [SerializeField] private float fadeWaitTime;
+    [SerializeField] private Renderer renderer;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Coroutine1());
     }
 
-    public IEnumerator Coroutine()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Fade());
+        }
+    }
+
+    private IEnumerator Coroutine()
     {
         print("<color=green> Ik start nu de coroutine </color>");
         yield return new WaitForSeconds(.5f);
@@ -20,7 +30,7 @@ public class Coroutines : MonoBehaviour
         yield return null;
     }
     
-    public IEnumerator Coroutine1()
+    private IEnumerator Coroutine1()
     {
         print("<color=green> Ik start nu de coroutine </color>");
         yield return new WaitForSeconds(.5f);
@@ -30,6 +40,21 @@ public class Coroutines : MonoBehaviour
             yield return new WaitForSeconds(.5f);
         }
         print("<color=red> coroutine einde </color>");
+        yield return null;
+    }
+
+    private IEnumerator Fade()
+    {
+        print("<color=green> Ik start nu met fade </color>");
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            Color c = renderer.material.color;
+            c.a = ft;
+            renderer.material.color = c;
+            print("<color=yellow> Fading </color>");
+            yield return new WaitForSeconds(fadeWaitTime);
+        }
+        print("<color=red> Stop fading </color>");
         yield return null;
     }
 }
