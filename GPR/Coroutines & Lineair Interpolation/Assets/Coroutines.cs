25525656a1032b13fbf5f51719bb27e3f693aa6e
Emile <emiledavidson@gmail.com>
2021-02-08 13:11:52 +0100

    added images & videos from results

--- GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
+++ GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
@@ -13,6 +13,7 @@ public class Coroutines : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // StartCoroutine(Coroutine1());
     }
 
     private void Update()
@@ -37,7 +38,7 @@ public class Coroutines : MonoBehaviour
     {
         print("<color=green> Ik start nu de coroutine </color>");
         yield return new WaitForSeconds(.5f);
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
             print("<color=yellow> coroutine update </color>");
             yield return new WaitForSeconds(.5f);
