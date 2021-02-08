e093def6a6e1f14c91e5d198a0350182211c9963
Emile <emiledavidson@gmail.com>
2021-02-08 12:07:00 +0100

    second function for asignment finished

--- GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
+++ GPR/Coroutines & Lineair Interpolation/Assets/Coroutines.cs
@@ -7,7 +7,7 @@ public class Coroutines : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Coroutine());
+        StartCoroutine(Coroutine1());
     }
 
     public IEnumerator Coroutine()
@@ -16,6 +16,19 @@ public class Coroutines : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         print("<color=yellow> coroutine update </color>");
         yield return new WaitForSeconds(.5f);
+        print("<color=red> coroutine einde </color>");
+        yield return null;
+    }
+    
+    public IEnumerator Coroutine1()
+    {
+        print("<color=green> Ik start nu de coroutine </color>");
+        yield return new WaitForSeconds(.5f);
+        for (int i = 0; i < 10; i++)
+        {
+            print("<color=yellow> coroutine update </color>");
+            yield return new WaitForSeconds(.5f);
+        }
         print("<color=red> coroutine einde </color>");
         yield return null;
     }
